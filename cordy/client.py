9e2dd3d530e065aed1ba6d813ea0289d0e891da9
Nemika <nemika@bytestobits.dev>
2021-08-31 20:31:48 +0300

    Create client.py

---
+++ cordy/client.py
@@ -0,0 +1,16 @@
+__all__ = (
+    'Client'
+)
+
+class Client:
+    def __init__(self):
+        ...
+
+    def connect(self, token):
+        ...
+
+    def disconnect(self):
+        ...
+
+    def reconnect(self):
+        ...
