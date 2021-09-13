e3d8fa30fae508ed4ebdfb72b3334c4083ffcfa0
WizzyGeek <51919967+WizzyGeek@users.noreply.github.com>
2021-09-13 18:35:50 +0530

    [🛣️] Make Route base impl.

--- cordy/http.py
+++ cordy/http.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from yarl import URL
 
 from typing import ClassVar
@@ -6,8 +8,40 @@ __all__ = (
     "Route"
 )
 
+# TODO: Make buckets for ratelimits
+# MAYBE: Remove cache if global instances are used
+
+class Endpoint:
+    __slots__ = ("route", "url")
+
+    route: Route
+    url: URL
+
+    def __init__(self, route: Route, params: dict[str, str]) -> None:
+        self.route = Route
+        self.url = self.route.BASE / route.path.format_map(params)
+
+    @property
+    def method(self) -> str:
+        return self.route.method
+
 class Route:
+    _CACHE: ClassVar[dict[str, Route]] = {}
     BASE: ClassVar[URL] = URL('https://discord.com/api/v8')
 
-    def __init__(self):
-        ...
+    __slots__ = ("method", "path")
+
+    def __new__(cls, method: str, path: str) -> Route:
+        cache_bucket = method + ":" + path
+        self = cls._CACHE.get(cache_bucket, None)
+
+        if self is None:
+            self = super().__new__(cls, method, path)
+            self.method = method
+            self.path = path
+            cls._CACHE[cache_bucket] = self
+
+        return self
+
+    def __call__(self, **params) -> Endpoint:
+        return Endpoint(self, params)
