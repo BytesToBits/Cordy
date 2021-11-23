e4bef66f468144adaffe45595bac40efdef18854
WizzyGeek <51919967+WizzyGeek@users.noreply.github.com>
2021-11-23 19:48:42 +0530

    [➕] Add errors for REST API

--- cordy/errors.py
+++ cordy/errors.py
@@ -11,3 +11,10 @@ class CordyError(Exception):
 class RateLimitTooLong(CordyError):
     """Raised when rate limit is too long to be passed by sleeping."""
     pass
+
+class MissingPermissions(CordyError):
+    """Raised when an endpoint requiring permission is requested"""
+    pass
+
+class InvalidAuth(CordyError):
+    """Raised when an invalid token is used in a request"""
